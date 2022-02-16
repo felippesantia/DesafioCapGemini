5e4a95bb20c053918d175f0cd125509c91acaa5b
felippesantia <felippesantia@gmail.com>
2022-02-15 16:36:02 -0800

    primeiro commit

---
+++ src/main/java/org/teste/escadaCapGemini/escadaCapGemini.java
@@ -0,0 +1,27 @@
+package org.teste.escadaCapGemini;
+
+public class escadaCapGemini {
+
+    public static void main(String[] args) {
+        escada(6);
+
+    }
+
+    public final static void escada(int n){
+
+        int tamanho = n;
+
+
+        for (int altura = 1; altura <= tamanho; altura++) {
+            for (int degrau = 1; degrau <= tamanho; degrau++) {
+                if(tamanho-altura >= degrau)
+                    System.out.print(" ");
+                else
+                    System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+    }
+
+}
